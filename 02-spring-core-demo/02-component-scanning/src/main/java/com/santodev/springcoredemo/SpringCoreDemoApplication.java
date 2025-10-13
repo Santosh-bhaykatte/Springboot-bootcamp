43512005716872659b29fff8c9a219b8615db3e8
Santosh Bhaykatte <bhaykatte2020@gmail.com>
2025-10-14 00:05:25 +0530

    understand component scanning

---
+++ 02-spring-core-demo/02-component-scanning/src/main/java/com/santodev/springcoredemo/SpringCoreDemoApplication.java
@@ -0,0 +1,22 @@
+package com.santodev.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.santodev.springcoredemo",
+                "com.santodev.util"
+        }
+)
+ */
+
+@SpringBootApplication
+public class SpringCoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreDemoApplication.class, args);
+	}
+
+}
---
+++ 02-spring-core-demo/02-component-scanning/src/main/java/com/santodev/springcoredemo/rest/DemoController.java
@@ -0,0 +1,22 @@
+package com.santodev.springcoredemo.rest;
+
+import com.santodev.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    //private field for dependency
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(Coach myCoach) {
+        this.myCoach = myCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
