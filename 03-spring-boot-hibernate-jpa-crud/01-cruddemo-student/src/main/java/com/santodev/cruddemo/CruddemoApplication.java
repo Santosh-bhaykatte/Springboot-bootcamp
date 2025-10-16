3bf8fbc1ed168a7d7e62c526c9ffbe19ebb1330c
Santosh Bhaykatte <bhaykatte2020@gmail.com>
2025-10-16 19:14:05 +0530

    database setup for crud

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/santodev/cruddemo/CruddemoApplication.java
@@ -0,0 +1,31 @@
+package com.santodev.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+    // executed after the spring beans have been loaded
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+        return runner -> {
+            System.out.println("Hello World");
+        };
+    }
+
+    /*
+    @Override
+    public void run(String... args) {
+        System.out.println("Hello World");
+    }
+
+     */
+
+}
