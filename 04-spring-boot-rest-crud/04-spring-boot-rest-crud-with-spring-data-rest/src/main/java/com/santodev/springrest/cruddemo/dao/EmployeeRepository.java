7e4bab958be5e815809138d3aa3bfec28f9eea19
Santosh Bhaykatte <bhaykatte2020@gmail.com>
2025-10-19 14:51:33 +0530

    Spring Data REST Pagination, sorting & configuration

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-with-spring-data-rest/src/main/java/com/santodev/springrest/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-with-spring-data-rest/src/main/java/com/santodev/springrest/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.santodev.springrest.cruddemo.dao;
 
 import com.santodev.springrest.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // No need to write any code... LOL!
 }
