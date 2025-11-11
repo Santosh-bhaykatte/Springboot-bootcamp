088b009a7e5d914fbbbac46ba518ed806c47cc3e
Santosh Bhaykatte <bhaykatte2020@gmail.com>
2025-11-11 14:29:44 +0530

    experimented with @RequestParam

--- 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,15 +1,17 @@
 package com.springboot.demo.mycoolapp.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/myapp")
 public class FunRestController {
     // expose '/hello' that return "Hello World"
 
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+        return String.format("Hello, %s!", name);
     }
 }
